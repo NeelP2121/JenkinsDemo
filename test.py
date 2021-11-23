629faeaa6a6b85ca02fbcd23591337e7c684b603
NeelP2121 <54849127+NeelP2121@users.noreply.github.com>
2021-11-23 11:38:29 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,23 +5,23 @@ from code import *
 
 class Test(unittest.TestCase):
     def test_addition(self):
-        data = [5, 10]
-        expected = 15
+        data = [6, 24]
+        expected = 30
         self.assertEqual(addition(data[0], data[1]), expected)
 
     def test_subtraction(self):
-        data = [5, 10]
-        expected = -5
+        data = [6, 24]
+        expected = -18
         self.assertEqual(subtraction(data[0], data[1]), expected)
 
     def test_multiplication(self):
-        data = [5, 10]
-        expected = 50
+        data = [6, 24]
+        expected = 144
         self.assertEqual(multiplication(data[0], data[1]), expected)
 
     def test_division(self):
-        data = [10, 2]
-        expected = 5
+        data = [24, 12]
+        expected = 2
         self.assertAlmostEqual(division(data[0], data[1]), expected)
 
 if __name__ == '__main__':
